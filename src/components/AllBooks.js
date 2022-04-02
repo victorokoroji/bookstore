d5a2e2bbd290f4e15c63c9e2b07783f788eca446
unknown <okorojiebube2@gmail.com>
2022-04-02 14:37:02 -0600

    Add books component

---
+++ src/components/AllBooks.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Book from './Book'
+import store from '../redux/configureStore'
+
+const AllBooks = () => {
+	const {
+		books: { books },
+	} = store.getState()
+	const Books = books.map(book => {
+		const { title, author, category, completed } = book
+		return (
+			<Book
+				key={Math.random()}
+				completed={completed}
+				title={title}
+				author={author}
+				category={category}
+			/>
+		)
+	})
+
+	return <div>{Books}</div>
+}
+
+export default AllBooks
