98308cbacd3a25508481902ff4c08ba80856d69c
unknown <okorojiebube2@gmail.com>
2022-04-05 16:18:22 -0600

    Modify Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Navbar from './Navbar';
 
 const Header = () => (
-  <div>
-    <Navbar />
-  </div>
-);
+	<div>
+		<h1>Bookstore CMS</h1>
+		<Navbar />
+	</div>
+)
 
 export default Header;
