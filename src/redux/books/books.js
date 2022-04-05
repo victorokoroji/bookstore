c0664ee23a512ded4f64259e9556d1fd128ea247
unknown <okorojiebube2@gmail.com>
2022-04-05 08:51:19 -0600

    Add bookReducer function

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,6 +1,10 @@
 const ADD_BOOK = 'bookstore/book/ADD_BOOK'
 const REMOVE_BOOK = 'bookstore/book/REMOVE_BOOK'
 
+const initialState = {
+	books: [],
+}
+
 export const addBook = book => {
 	return {
 		type: ADD_BOOK,
@@ -12,5 +16,24 @@ export const removeBook = id => {
 	return {
 		type: REMOVE_BOOK,
 		payload: id,
+	}
+}
+
+const bookReducer = (state = initialState, action={}) => {
+  const { payload } = action
+	switch (action.type) {
+		case ADD_BOOK:
+			return {
+				...state,
+				books: payload,
+			}
+		case REMOVE_BOOK:
+			return {
+				...state,
+				books: payload,
+			}
+
+		default:
+			return state
 	}
 }
