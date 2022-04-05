4c74c949f849791344b16961918abaeeccb68288
unknown <okorojiebube2@gmail.com>
2022-04-05 16:17:44 -0600

    Modify Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,20 @@
 import React from 'react'
 
 const Navbar = () => {
-
 	return (
-		<nav>
-			<div className='navBar'>
-				<h1>Bookstore CMS</h1>
-				<nav>
-					<ul>
-						<li>
-							<NavLink to='/'>BOOKS</NavLink>
-						</li>
-						<li>
-							<NavLink to='Categories'>CATEGORY</NavLink>
-						</li>
-					</ul>
-					<div className='icon'>.</div>
-				</nav>
-			</div>
+		<nav className='navBar'>
+				<ul>
+					<li>
+						<NavLink to='/' exact className={navLink => (navLink.isActie ? 'active' : 'none')}>
+							BOOKS
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='Categories' className={navLink => (navLink.isActie ? 'active' : 'none')}>
+							CATEGORY
+						</NavLink>
+					</li>
+				</ul>
 		</nav>
 	)
 }
