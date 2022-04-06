9212d5d613920bc828efd203bcd0083428f91a9d
unknown <okorojiebube2@gmail.com>
2022-04-06 17:33:53 -0600

    Add Provider component to root App component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import App from './App'
+import store from './redux/configureStore'
 
 ReactDOM.render(
-  <React.StrictMode>
+	<React.StrictMode>
     <BrowserRouter>
+      <Provider store = {store}>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+      </Provider>
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById('root'),
+)
