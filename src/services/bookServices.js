9187a52a0c67d8a81100ea79c141c3703ffc34b6
unknown <okorojiebube2@gmail.com>
2022-04-07 14:11:54 -0600

    Add remove book from  server  function

--- src/services/bookServices.js
+++ src/services/bookServices.js
@@ -5,44 +5,45 @@ const getFromServer = async () => {
 		const res = await axios.get(
 			'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/UT7NQsHrRxIY6vwPxxrv/books/',
 		)
-    const data = res.data
-    console.log(data)
-    return data
-  } catch (error) {
-    return error
-  }
+		const data = res.data
+		console.log(data)
+		return data
+	} catch (error) {
+		return error
+	}
 }
 
-const sendToServer = async (datas) => {
+const sendToServer = async datas => {
 	try {
 		const res = await axios.get(
-			'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/UT7NQsHrRxIY6vwPxxrv/books/', {datas}
+			'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/UT7NQsHrRxIY6vwPxxrv/books/',
+			{ datas },
 		)
-    const data = res.data
-    console.log(data)
-    return data
-  } catch (error) {
-    return error
-  }
+		const data = res.data
+		console.log(data)
+		return data
+	} catch (error) {
+		return error
+	}
 }
 
-const removeFromServer = async (id) => {
+const removeFromServer = async id => {
 	try {
 		const res = await axios.delete(
-			'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/UT7NQsHrRxIY6vwPxxrv/books/', {datas}
+			`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/UT7NQsHrRxIY6vwPxxrv/books/${id}`,
 		)
-    const data = res.data
-    console.log(data)
-    return data
-  } catch (error) {
-    return error
-  }
+		const data = res.data
+		console.log(data)
+		return data
+	} catch (error) {
+		return error
+	}
 }
 
 const bookServices = {
-  getAllBooks,
-  postBooks,
-
+	getFromServer,
+  sendToServer,
+  removeFromServer
 }
 
 export default bookServices
