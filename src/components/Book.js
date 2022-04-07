84a825c7abbc96f1c104b7b41316b99b33f4854a
unknown <okorojiebube2@gmail.com>
2022-04-06 20:40:37 -0600

    Remove book

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import propTypes from 'prop-types';
-import Button from './Button';
+import React from 'react'
+import propTypes from 'prop-types'
+import { useDispatch } from 'react-redux'
+import Button from './Button'
+import { removeBook } from '../redux/books/books'
 
-const Book = ({book}) => (
-  <>
-    <ul>
-      <li>{book.title}</li>
-      <li>{book.author}</li>
-    </ul>
-    <Button>Remove</Button>
-  </>
-);
+const Book = ({ book }) => {
+	const dispatch = useDispatch()
+	const handleDelete = () => {
+		dispatch(removeBook(book.id))
+		document.querySelector(`#${book.id}`).remove()
+	}
+	return (
+		<div className={book.id}>
+			<ul>
+				<li>{book.title}</li>
+				<li>{book.author}</li>
+			</ul>
+			<Button onClick={handleDelete}>Remove</Button>
+		</div>
+	)
+}
 
 Book.propTypes = {
-  title: propTypes.string.isRequired,
-  author: propTypes.string.isRequired,
-};
+	title: propTypes.string.isRequired,
+	author: propTypes.string.isRequired,
+}
 
-export default Book;
+export default Book
