36c3c2641a9533bf92e1a4e6dedd11ea794297a3
unknown <okorojiebube2@gmail.com>
2022-04-07 13:11:06 -0600

    Install redux-thunk

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,43 +1,41 @@
-import { v4 as uuidV4 } from 'uuid';
+import { v4 as uuidV4 } from 'uuid'
 
-const ADD_BOOK = 'bookstore/book/ADD_BOOK';
-const REMOVE_BOOK = 'bookstore/book/REMOVE_BOOK';
+const ADD_BOOK = 'bookstore/book/ADD_BOOK'
+const REMOVE_BOOK = 'bookstore/book/REMOVE_BOOK'
 
 const initialState = [
-  {
-    id: uuidV4(),
-    title: 'Romeo and Juliet',
-    author: 'Will Smith',
-  },
-  {
-    id: uuidV4(),
-    title: 'Things fall apart',
-    author: 'Chinua Achebe',
-  },
-];
+	{
+		id: uuidV4(),
+		title: 'Romeo and Juliet',
+		author: 'Will Smith',
+	},
+	{
+		id: uuidV4(),
+		title: 'Things fall apart',
+		author: 'Chinua Achebe',
+	},
+]
 
-export const addBook = (book) => ({
-  type: ADD_BOOK,
-  payload: book,
-});
+export const addBook = book => ({
+	type: ADD_BOOK,
+	payload: book,
+})
 
-export const removeBook = (id) => ({
-  type: REMOVE_BOOK,
-  payload: id,
-});
+export const removeBook = id => ({
+	type: REMOVE_BOOK,
+	payload: id,
+})
 
 const bookReducer = (state = initialState, action) => {
-  const { payload } = action;
-  switch (action.type) {
-    case ADD_BOOK:
-      return [
-        ...state,
-        payload,
-      ];
-    case REMOVE_BOOK:
-      return state.filter((book) => book.id !== payload);
-    default:
-      return state;
-  }
-};
-export default bookReducer;
+	const { payload } = action
+	switch (action.type) {
+		case ADD_BOOK:
+			return [...state, payload]
+		case REMOVE_BOOK:
+			return state.filter(book => book.id !== payload)
+		default:
+			return state
+	}
+}
+export default bookReducer
+
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,9 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import bookReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
 const rootReducer = combineReducers({ bookReducer, categoriesReducer });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
