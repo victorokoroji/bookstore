97a51dc0dceee264bf4cc0bf10ab1c18871e4f81
unknown <okorojiebube2@gmail.com>
2022-04-07 21:25:11 -0600

    Modify addBook action

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,64 +1,66 @@
-import bookServices from '../../services/bookServices';
+import bookServices from '../../services/bookServices'
 
-const GET_BOOKS = 'bookstore/book/GET_BOOKS';
-const ADD_BOOK = 'bookstore/book/ADD_BOOK';
-const REMOVE_BOOK = 'bookstore/book/REMOVE_BOOK';
+const GET_BOOKS = 'bookstore/book/GET_BOOKS'
+const ADD_BOOK = 'bookstore/book/ADD_BOOK'
+const REMOVE_BOOK = 'bookstore/book/REMOVE_BOOK'
 
-const initialState = [];
+const initialState = []
 
-export const getBooks = () => async (dispatch) => {
-  try {
-    const result = await bookServices.getFromServer();
-    return dispatch({
-      type: GET_BOOKS,
-      payload: result,
-    });
-  } catch (error) {
-    return error;
-  }
-};
+export const getBooks = () => async dispatch => {
+	try {
+		const result = await bookServices.getFromServer()
+		return dispatch({
+			type: GET_BOOKS,
+			payload: result,
+		})
+	} catch (error) {
+		return error
+	}
+}
 
-export const addBook = (book) => async (dispatch) => {
-  try {
-    const result = await bookServices.sendToServer(book);
-    return dispatch({
-      type: ADD_BOOK,
-      payload: result,
-    });
-  } catch (error) {
-    return error;
-  }
-};
+export const addBook = book => async dispatch => {
+	const result = await bookServices.sendToServer(book)
+	if (result === 'Created') {
+		dispatch({
+			type: ADD_BOOK,
+			payload: {
+				id: book.item_id,
+				title: book.title,
+				author: book.author,
+			},
+		})
+	}
+}
 
-export const removeBook = (id) => async (dispatch) => {
-  try {
-    await bookServices.removeFromServer(id);
-    return dispatch({
-      type: REMOVE_BOOK,
-      payload: id,
-    });
-  } catch (error) {
-    return error;
-  }
-};
+export const removeBook = id => async dispatch => {
+	try {
+		await bookServices.removeFromServer(id)
+		return dispatch({
+			type: REMOVE_BOOK,
+			payload: id,
+		})
+	} catch (error) {
+		return error
+	}
+}
 
 const bookReducer = (state = initialState, action) => {
-  const { payload } = action;
-  switch (action.type) {
-    case GET_BOOKS:
-      return [
-        ...Object.entries(payload).map((item) => ({
-          id: item[0],
-          title: item[1][0].title,
-          author: item[1][0].author,
-        })),
-      ];
-    case ADD_BOOK:
-      return [...state, payload];
-    case REMOVE_BOOK:
-      return state.filter((book) => book.id !== payload);
-    default:
-      return state;
-  }
-};
-export default bookReducer;
+	const { payload } = action
+	switch (action.type) {
+		case GET_BOOKS:
+			return [
+				...Object.entries(payload).map(item => ({
+					id: item[0],
+					title: item[1][0].title,
+					author: item[1][0].author,
+				})),
+			]
+		case ADD_BOOK:
+			return [...state, payload]
+		case REMOVE_BOOK:
+			return state.filter(book => book.id !== payload)
+		default:
+			return state
+	}
+}
+export default bookReducer
