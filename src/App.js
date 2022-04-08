7d3542664861bba187260bbae23d49bd0e85e697
unknown <okorojiebube2@gmail.com>
2022-04-08 07:05:20 -0600

    Add global styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import BooksPage from './pages/BooksPage';
-import CategoryPage from './pages/CategoryPage';
-import Footer from './components/Footer';
-import Header from './components/Header';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import BooksPage from './pages/BooksPage'
+import CategoryPage from './pages/CategoryPage'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 const App = () => (
-  <div className="main">
-    <section>
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<BooksPage />} />
-        <Route path="/categories" element={<CategoryPage />} />
-      </Routes>
-    </section>
-    <section>
-      <Footer />
-    </section>
-  </div>
-);
+	<>
+		<Header />
+		<Routes>
+			<Route exact path='/' element={<BooksPage />} />
+			<Route path='/categories' element={<CategoryPage />} />
+		</Routes>
+		<section>
+			<Footer />
+		</section>
+	</>
+)
 
-export default App;
+export default App
